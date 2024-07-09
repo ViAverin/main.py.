6f31e09afc22396b424a3030d063842adb4b7086
Victor Averin <victor.cybernetic@gmail.com>
2024-07-09 21:51:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1st programm
+print(9 ** 0.5 * 5)
+
+# 2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+
+# 4th programm
+x, y = 13.42, 42.13
+xi, yi = int(x), int(y)
+xf = int((x - xi) * 100)
+yf = int((y - yi) * 100)
+print(xi == yf)
+print(int((x - xi) * 100 + 1) == yi)
